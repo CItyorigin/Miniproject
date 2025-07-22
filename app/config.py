aa6692600a23b5d70b56d6e07bfab02133f05649
Cityorigin <dksem159@gmail.com>
2025-07-22 15:21:22 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,18 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    # API 관련 설정
+    API_TITLE = "My API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.1.3"
+    OPENAPI_URL_PREFIX = "/api"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+    #데이터베이스 설정
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 10
+    SQLALCHEMY_POOL_TIMEOUT = 5
+    SQLALCHEMY_POOL_RECYCLE = 1800
+    SQLALCHEMY_MAX_OVERFLOW = 5
+    SQLALCHEMY_ECHO = False
+    reload = True
